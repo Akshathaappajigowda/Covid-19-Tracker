845e7e936ce614ec30263dce494c544be542e2ea
Akshathaappajigowda <akshathaa0108@gmail.com>
2020-10-20 23:21:50 +0200

    Map is working fine

--- src/App.js
+++ src/App.js
@@ -115,12 +115,12 @@ const App = () => {
             total={numeral(countryInfo.deaths).format("0.0a")}
           />
         </div>
-        {/* <Map
+        <Map
           countries={mapCountries}
           casesType={casesType}
           center={mapCenter}
           zoom={mapZoom}
-        /> */}
+        /> 
       </div>
       <Card className="app__right">
         <CardContent>
